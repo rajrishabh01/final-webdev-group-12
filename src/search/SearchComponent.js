684a197a9c5d5404ab211da4746a2c17458a2da2
wantingzeng <zeng.wan@northeastern.edu>
2022-12-06 12:55:04 -0500

    search recipes by ingredient with api

--- src/search/SearchComponent.js
+++ src/search/SearchComponent.js
@@ -1,9 +1,25 @@
 import React from "react";
+import SearchBar from "./search-bar";
 
 const SearchComponent = () => {
     return(
         <div>
             <h1>Search</h1>
+            <div className="row">
+                <div className="col-2">
+
+                </div>
+                <div className="col">
+                    <SearchBar/>
+                </div>
+                <div className="col-2">
+
+                </div>
+
+            </div>
+
+
+
         </div>
     );
 
