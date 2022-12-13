87520e520a7ae777128a24fb299f4846eb9ac580
Vasu Agarwal <vasu@Vasus-MacBook-Pro.local>
2022-12-13 17:46:23 -0500

    Added styling to home and create recipe pages

--- src/Home/HomeComponent.js
+++ src/Home/HomeComponent.js
@@ -2,11 +2,12 @@
 import React from "react";
 import CreateRecipesComponent from "../recipe/create-recipe";
 import {useSelector} from "react-redux";
+import './index.css';
 
 const HomeComponent = () => {
     const { currentUser } = useSelector((state) => state.users);
     return (
-        <div className="ms-3">
+        <div className="ps-3 wd-home-bg">
             {
                 currentUser &&
                 <h2>Welcome {currentUser.firstName}!</h2>
